2134701e6e66da7369f0a8bddc7e440726ab51a2
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2024-05-27 22:21:22 +0300

    added cell image change

--- src/Cells/DummyCell.ts
+++ src/Cells/DummyCell.ts
@@ -3,11 +3,30 @@ import { Cell } from "./Cell.js";
 export class DummyCell implements Cell {
     picture: string;
 
+    images: string[] = [
+        "../pictures/arrow_up.png",
+        "../pictures/arrow_right.png",
+        "../pictures/arrow_down.png",
+        "../pictures/arrow_left.png",
+        "../pictures/default_cell.png",
+    ];
+    currentIndex: number = 0;
+
     constructor(picture: string = "") {
         this.picture = picture;
     }
 
     action(x: number, y: number): void {
-        alert(`Clicked cell at (${x}, ${y})!`);
-    }
+        const cellElement = document.querySelector(`.cell[data-x="${x}"][data-y="${y}"]`);
+        if (cellElement) {
+            const imageUrl = this.images[this.currentIndex];
+    
+            const img = document.createElement('img');
+            img.src = imageUrl;
+            cellElement.innerHTML = '';
+            cellElement.appendChild(img);
+
+            this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        }
+    }    
 }
