a14b12b67631cb0298854bab5ff0f109f3b25ab5
Maksym Diachok <maksdyachok2005@gmail.com>
2024-05-29 20:17:02 +0300

    Add arrow

--- test/human.test.ts
+++ test/human.test.ts
@@ -1,13 +1,27 @@
 import Field from '../src/Field.js';
 import { Cell, Human } from '../src/Cells/Cells.js';
-import Vector from './Vector.js';
+import Vector from '../src/Vector.js';
 import { expect } from 'chai';
+import { ArrowCell } from '../src/Cells/ArrowCell.js';
+import { Direction } from '../src/Direction.js';
 
 describe('Human', () => {
   it ('should walk', function () {
       const field = new Field<Cell>(10, 10);
       const human = new Human(field, 0, 0);
+      human.moveVector = new Vector(0, 1);
       human.action();
-      expect(human.position).to.deep.equal(new Vector(0, 1));
+      human.action();
+      expect(human.position).to.deep.equal(new Vector(0, 2));
+  })
+
+  it ('should change direction on arrow', function () {
+      const field = new Field<Cell>(10, 10);
+      const human = new Human(field, 0, 0);
+      const arrow = new ArrowCell(field, 0, 1, Direction.RIGHT);
+      human.moveVector = new Vector(0, 1);
+      human.action();
+      human.action();
+      expect(human.position).to.deep.equal(new Vector(1, 1));
   })
 });
