b1ecf9ac572ff34444f2d9f9b7b320f64e75f3a1
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2024-06-02 11:53:14 +0300

    changed text on "change speed" button to pictures

--- src/index.ts
+++ src/index.ts
@@ -46,13 +46,13 @@ const scaleStep = 0.01;
 
 let timerSpeed = 1000;
 const speeds = [
-  { value: 1000, name: '1x' },
-  { value: 750, name: '1.5x' },
-  { value: 500, name: '2x' },
-  { value: 250, name: '2.5x' },
-  { value: 2500, name: '0.25x' },
-  { value: 2000, name: '0.5x' },
-  { value: 1500, name: '0.75x' },
+  { value: 1000, name: '1x', picture: './pictures/1x_speed.png' },
+  { value: 750, name: '1.5x', picture: './pictures/1.5x_speed.png' },
+  { value: 500, name: '2x', picture: './pictures/2x_speed.png' },
+  { value: 250, name: '2.5x', picture: './pictures/2.5x_speed.png' },
+  { value: 2500, name: '0.25x', picture: './pictures/0.25x_speed.png' },
+  { value: 2000, name: '0.5x', picture: './pictures/0.5x_speed.png' },
+  { value: 1500, name: '0.75x', picture: './pictures/0.75x_speed.png' },
 ];
 let currentSpeedIndex = 0;
 let timerId: number | null = null;
@@ -412,8 +412,8 @@ function handleResetField() {
 function handleTimerMoveSpeed() {
   currentSpeedIndex = (currentSpeedIndex + 1) % speeds.length;
   timerSpeed = speeds[currentSpeedIndex].value;
-  document.getElementById('buttonMoveSpeed')!.innerText =
-    speeds[currentSpeedIndex].name;
+  document.getElementById('buttonMoveSpeed')!.querySelector('img')!.src =
+    speeds[currentSpeedIndex].picture;
   if (timerId !== null) {
     stopTimer();
     startTimer();
